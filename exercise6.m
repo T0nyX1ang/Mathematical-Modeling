cb48963493be1666e275ab94f59df2dcd2c9196f
Tony Xiang <33901373+T0nyX1ang@users.noreply.github.com>
2018-07-18 15:10:32 +0800

    Delete exercise6.m

--- exercise6.m
+++
@@ -1,8 +0,0 @@
-% Exercise 6 - Array Functions and Spot
-% Edited by Tony Xiang
-% Last Modified in 2018-7-13
-
-int_fun = @(t) (1 - t .^ 2) .^ (2/3);
-x = linspace(0, 4 * pi);
-y = arrayfun(@(x) integral(int_fun, 0, sin(x)), x);
-plot(x, y)
