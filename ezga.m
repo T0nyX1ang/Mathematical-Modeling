ffb429d31ff6f4914036a7a966455d721de9dc2a
Tony Xiang <33901373+T0nyX1ang@users.noreply.github.com>
2018-07-21 10:37:43 +0800

    Update ezga.m
    Easy GA Version 1.1 Comes

--- ezga.m
+++ ezga.m
@@ -1,30 +1,26 @@
-function [bestx, optimal] = ezga(fun, nvars, lb, ub, lbin, ubin)
-    %         CHANGE THE FUNCTION FIRST IF YOU WANT A MAXIMUM SEARCH
+function [bestx, optimal] = ezga(fun, nvars, lb, ub)
+    % CHANGE THE FUNCTION FIRST IF YOU WANT A MAXIMUM SEARCH
 
     % Configuration
-    PopulationNumber = 400;
+    PopulationNumber = 40;
     MaxGeneration = 100;
     GenerationGap = 0.95;
     CrossoverProb = 0.7;
     MutationProb = 0.01;
-    PersonLength = 200;
     SUBPOP = 1;
     InsertOption = 1; % 0 for uniform choice, 1 for fitness value, 2 for ratio value
     SEL_Function = 'sus'; % rws or sus
     REC_Function = 'xovsp'; % recdis or xovsp
-    PersonMatrix = PersonLength * ones(1, nvars);
-    CodingMethod = 1 * ones(1, nvars); % 0 for Gray code; 1 for binary code
-    ScaleMethod = 0 * ones(1, nvars); % 0 for numeric code; 1 for logarithm code
     DrawEvolutionTable = 1;
 
     % Initialization
     tracer = zeros(nvars + 1, MaxGeneration);
-    FieldDescriptor = [PersonMatrix; lb; ub; CodingMethod; ScaleMethod; lbin; ubin];
-    CreatePopulation = crtbp(PopulationNumber, PersonLength * nvars);
+    FieldDescriptor = [lb; ub];
+    CreatePopulation = crtrp(PopulationNumber, FieldDescriptor);
     
     % Optimization
     counter = 0;                                                                              
-    X = bs2rv(CreatePopulation, FieldDescriptor); % base change
+    X = CreatePopulation;
     ObjectValue = zeros(size(X, 1), 1);
     for i = 1:size(X, 1)
         ObjectValue(i) = fun(X(i, :)');
@@ -34,15 +30,18 @@ function [bestx, optimal] = ezga(fun, nvars, lb, ub, lbin, ubin)
         FitnessValue = ranking(ObjectValue); % define fitness value
         SelectPopulation = select(SEL_Function, CreatePopulation, FitnessValue, GenerationGap); % select population
         Recombination = recombin(REC_Function, SelectPopulation, CrossoverProb); % recombine
-        Mutation = mut(Recombination, MutationProb); % mutate
-        X = bs2rv(Mutation, FieldDescriptor); % base change
+        Mutation = mutbga(Recombination, FieldDescriptor, MutationProb); % mutate
+        
+        X = Mutation;
         ObjectValueNext = zeros(size(X, 1), 1);
         for i = 1:size(X, 1)
-            ObjectValueNext(i) = fun(X(i, :)');
+            ObjectValueNext(i) = fun(X(i, :)'); % generate son generation values
         end
+        
         [CreatePopulation, ObjectValue] = reins(CreatePopulation, Mutation, SUBPOP, ...
-                                                InsertOption, ObjectValue, ObjectValueNext);  % reinsert
-        X = bs2rv(CreatePopulation, FieldDescriptor); % update X
+                                                InsertOption, ObjectValue, ObjectValueNext);  % reinsert son to father
+
+        X = CreatePopulation;                                    
         counter = counter + 1; % update counter
         [Optimal, Index] = min(ObjectValue); % get minimal index
         tracer(1:nvars, counter) = X(Index, 1:nvars); % get minimal in every generation
@@ -62,5 +61,4 @@ function [bestx, optimal] = ezga(fun, nvars, lb, ub, lbin, ubin)
     % Output Value
     optimal = tracer(end, end);
     bestx = tracer(1:nvars, end);
-
 end
