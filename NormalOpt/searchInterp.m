8db2d62403ce1ea2d6064770b1bb8169b0e61815
Tony Xiang <33901373+T0nyX1ang@users.noreply.github.com>
2019-01-22 00:00:51 +0800

    Update NormalOpt

--- NormalOpt/searchInterp.m
+++ NormalOpt/searchInterp.m
@@ -5,18 +5,18 @@ function point = searchInterp(funct, start, stop, epsilon)
     end
     
     tangent = getTangent(funct);
-    if (stop < start) || (tangent(start) >= 0) || (tangent(stop) <= 0)
+    if (stop < start) || (tangent{1}(start) >= 0) || (tangent{1}(stop) <= 0)
         error("Invalid initial condition");
     end
     
     while (abs(stop - start) > epsilon)
         s = 3 * (funct(stop) - funct(start)) / (stop - start);
-        z = s - tangent(stop) - tangent(start);
-        w = sqrt(z .^ 2 - tangent(start) .* tangent(stop));
-        next = start + (stop - start) * (1 - (tangent(stop) + w + z) / (tangent(stop) - tangent(start) + 2 * w));
-        if (tangent(next) < 0)
+        z = s - tangent{1}(stop) - tangent{1}(start);
+        w = sqrt(z .^ 2 - tangent{1}(start) .* tangent{1}(stop));
+        next = start + (stop - start) * (1 - (tangent{1}(stop) + w + z) / (tangent{1}(stop) - tangent{1}(start) + 2 * w));
+        if (tangent{1}(next) < 0)
             start = next;
-        elseif (tangent(next) > 0)
+        elseif (tangent{1}(next) > 0)
             stop = next;
         else
             break;
