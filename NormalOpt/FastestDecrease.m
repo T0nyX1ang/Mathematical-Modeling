425956c1246541adbbf4f148d85d89b9783ef6de
Tony Xiang <33901373+T0nyX1ang@users.noreply.github.com>
2019-01-22 15:00:52 +0800

    Change a variable's name

--- NormalOpt/FastestDecrease.m
+++ NormalOpt/FastestDecrease.m
@@ -1,15 +1,15 @@
-function [xval, fval] = FastestDecrease(funct, initial, eps, step)
+function [xval, fval] = FastestDecrease(funct, initial, epsilon, step)
     % An implementation of fastest decrease method.
-    % eps: error value
+    % epsilon: error value
     % step: search step when finding a valid interval
     if (nargin == 2)
-        eps = 1e-6;
+        epsilon = 1e-6;
         step = 1e-3;
     elseif (nargin == 3)
         step = 1e-3;
     end    
-    if (eps < 0)
-        error("eps must be greater than 0");
+    if (epsilon < 0)
+        error("epsilon must be greater than 0");
     end
     if (step <= 0)
         error("step must be greater than 0");
@@ -19,10 +19,10 @@ function [xval, fval] = FastestDecrease(funct, initial, eps, step)
     xval = initial;
     tangent = getTangent(funct, dimension);
     tval = getTangentValue(tangent, xval);
-    while (norm(tval) >= eps)
+    while (norm(tval) >= epsilon)
         dec_funct = @(lambda) funct(xval + lambda * tval);
         [start, stop] = searchValidInterval(dec_funct, 0, step);
-        lambda = searchGoldenMean(dec_funct, start, stop, eps);
+        lambda = searchGoldenMean(dec_funct, start, stop, epsilon);
         xval = xval + lambda * tval;
         tval = getTangentValue(tangent, xval);
     end
