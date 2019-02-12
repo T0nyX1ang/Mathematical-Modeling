21a2e8d0f1a0f7f6a2cd9153cab15ce74fd4dabd
Tony Xiang <33901373+T0nyX1ang@users.noreply.github.com>
2019-02-12 22:35:23 +0800

    Update Simplex Method, Step 1

--- NormalOpt/SimplexMethod.m
+++ NormalOpt/SimplexMethod.m
@@ -1,37 +1,47 @@
 function [xval, fval] = SimplexMethod(c, A, b)
-% Simplex method for linear programming.
-% This program is to solve functions like:
-% min f = cx such that Ax = b, x >= 0, b >= 0
+    % Simplex method for linear programming.
+    % This program is to solve functions like:
+    % min f = cx such that Ax = b, x >= 0, b >= 0
 
-if (rank(A) ~= size(A, 1)) || (size(c, 2) ~= size(A, 2)) || ...
-   (size(c, 1) ~= 1) || (size(A, 1) ~= size(b, 1)) || (size(b, 2) ~= 1)
-    error("Invalid matrix.");
-end
-
-% Find a solution first.
-extA = [A, eye(size(A, 1))];
-extB = b;
-extC = [zeros(size(c)), -ones(size(b')), 0];
-table = [extA extB; extC]; % Generate a table
-% Manipulate the table
-for i = 1:size(A, 1)
-    table(end, :) = table(end, :) + table(i, :);
-end
-% Pivot, step by step
-[val, cind] = max(table(end, 1: end - 1));
-while (val > 0)
-    col = table(1:end - 1, end) ./ table(1: end - 1, cind);
-    greater = find(col > 0);
-    [~, row] = min(col(greater)); 
-    rind = greater(row); % find row index
-    point = table(rind, cind); % find the point
-    table(rind, :) = table(rind, :) ./ point;
-    % Elimination
-    for i = 1:size(table, 1)
-        if (i ~= rind)
-            table(i, :) = table(i, :) - table(i, cind) .* table(rind, :); 
-        end
+    if (size(c, 2) ~= size(A, 2)) || (size(c, 1) ~= 1) || ...
+       (size(A, 1) ~= size(b, 1)) || (size(b, 2) ~= 1)
+        error("Invalid matrix.");
     end
-    [val, cind] = max(table(end, 1: end - 1));
+
+    % Find a solution first.
+    extA = [A, eye(size(A, 1))];
+    extB = b;
+    extC = [zeros(size(c)), -ones(size(b')), 0];
+    inX = size(c, 2) + 1: size(c, 2) + size(A, 1);
+    extTable = [extA extB; extC]; % Generate a table
+    % Manipulate the table
+    for i = 1:size(A, 1)
+        extTable(end, :) = extTable(end, :) + extTable(i, :);
+    end
+    % Pivot, step by step
+    [val, cind] = max(extTable(end, 1: end - 1));
+    while (val > 0)
+        col = extTable(1:end - 1, end) ./ extTable(1: end - 1, cind);
+        greater = find(col > 0);
+        [~, row] = min(col(greater)); 
+        rind = greater(row); % find row index
+        point = extTable(rind, cind); % find the point
+        inX(rind) = cind; % calculate inner columns at the same time
+        extTable(rind, :) = extTable(rind, :) ./ point;
+        % Elimination
+        for i = 1:size(extTable, 1)
+            if (i ~= rind)
+                extTable(i, :) = extTable(i, :) - extTable(i, cind) .* extTable(rind, :); 
+            end
+        end
+        [val, cind] = max(extTable(end, 1: end - 1));
+    end
+    if (extTable(end, end) ~= 0)
+        error("Unable to find a valid initial point.");
+    else
+        tempX = zeros(size(extTable, 2), 1);
+        tempX(inX) = extTable(1: end - 1, end);
+        initX = tempX(1: size(c, 2)); % get initial point of original LP
+    end
+    initX 
 end
-table
