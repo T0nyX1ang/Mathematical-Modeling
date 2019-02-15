162f88f91ccef5c59a0a4a013416855ec563622a
Tony Xiang <33901373+T0nyX1ang@users.noreply.github.com>
2019-02-15 23:51:01 +0800

    Update uniform simplex solver, step 2

--- NormalOpt/UniformSimplexConnector.m
+++ NormalOpt/UniformSimplexConnector.m
@@ -32,16 +32,16 @@ function [xval, fval] = UniformSimplexConnector(c, A, b, Aeq, beq, lb, ub, eps)
         ub = +inf(size(c));
     end
     
-    % create movement table
+    % create movement table for recovering the data later
     % line 1 demonstrates movement type 
     % value 0 for x - u type + v - x type
     % value 1 for x - u type
     % value 2 for v - x type
     % value 3 for x1 - x2 type
-    % line 2 demonstrates movement value
-    % line 3 demonstrates movement place
+    % line 2 demonstrates movement place
     % value n for place n
     % value -1 for not taking a place
+    
     movement_table = zeros(3, size(c, 2));
     place = size(c, 2) + 1;
     for i = 1: size(c, 2)
@@ -50,24 +50,53 @@ function [xval, fval] = UniformSimplexConnector(c, A, b, Aeq, beq, lb, ub, eps)
         end       
         if (lb(i) ~= -inf) && (ub(i) ~= +inf)
             movement_table(1, i) = 0;
-            movement_table(2, i) = ub(i) - lb(i);
-            movement_table(3, i) = place;
-            place = place + 1;
+            movement_table(2, i) = -1;
+            % Add constraint for full bounded condition
+            b(:) = b(:) + A(:, i) * lb(i);
+            temprow = zeros(size(c));
+            temprow(i) = 1;
+            A = [A; temprow];
+            b = [b; ub(i) - lb(i)];
         elseif (lb(i) ~= -inf) && (ub(i) == +inf)
             movement_table(1, i) = 1;
-            movement_table(2, i) = -lb(i);
-            movement_table(3, i) = -1;
+            movement_table(2, i) = -1;
+            b(:) = b(:) + A(:, i) * lb(i); 
         elseif (lb(i) == -inf) && (ub(i) ~= +inf)
             movement_table(1, i) = 2;
-            movement_table(2, i) = ub(i);
-            movement_table(3, i) = -1;
-        elseif (lb(i) ~= -inf) && (ub(i) ~= +inf)
+            movement_table(2, i) = -1;
+            b(:) = b(:) - A(:, i) * ub(i);
+            A(:, i) = -A(:, i);
+        elseif (lb(i) == -inf) && (ub(i) == +inf)
             movement_table(1, i) = 3;
-            movement_table(2, i) = 0;
-            movement_table(3, i) = place;
+            movement_table(2, i) = place;
             place = place + 1;
+            % Add variable for unbounded condition
+            c = [c, 0];
+            A = [A, -A(:, i)];
+            if (~isempty(Aeq))
+                Aeq = [Aeq, -Aeq(:, i)];
+            end
         end
     end
     
-    movement_table
+    % Add surplus variables for inequalities
+    for i = 1: size(A, 1)
+        tempcol = zeros(size(A, 1), 1);
+        if b(i) < 0
+            A(i, :) = -A(1, :);
+            b(i) = -b(i);
+            tempcol(i) = -1;
+            A = [A, tempcol];
+            c = [c, 0];
+        else
+            tempcol(i) = 1;
+            A = [A, tempcol];
+            c = [c, 0];
+        end
+        if (~isempty(Aeq))
+            Aeq = [Aeq, zeros(size(Aeq, 1), 1)];
+        end
+    end
+    c
+    check = [A, b; Aeq, beq]
 end
