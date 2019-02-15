729437beb2a14193e226cf83970cc24c2a7f5a4d
Tony Xiang <33901373+T0nyX1ang@users.noreply.github.com>
2019-02-15 22:51:26 +0800

    Update uniform simplex solver

--- NormalOpt/UniformSimplexConnector.m
+++ NormalOpt/UniformSimplexConnector.m
@@ -19,11 +19,55 @@ function [xval, fval] = UniformSimplexConnector(c, A, b, Aeq, beq, lb, ub, eps)
        (size(A, 1) ~= size(b, 1)) || (size(b, 2) ~= 1) || ...
        (~isempty(Aeq) && ~isempty(beq) && ((size(c, 2) ~= size(Aeq, 2)) || ...
        (size(Aeq, 1) ~= size(beq, 1)) || (size(beq, 2) ~= 1))) || ...
-       (~isempty(lb) && ~isempty(ub) && ((size(lb, 2) ~= 1) || (size(ub, 2) ~= 1) || ...
+       (~isempty(lb) && ~isempty(ub) && ((size(lb, 1) ~= 1) || (size(ub, 1) ~= 1) || ...
        (size(lb, 2) ~= size(c, 2)) || (size(ub, 2) ~= size(c, 2))))
         error("Invalid matrix.");
     end
     
     % Integrate lowerbounds and upperbounds into constraints
+    if (isempty(lb))
+        lb = -inf(size(c));
+    end
+    if (isempty(ub))
+        ub = +inf(size(c));
+    end
     
+    % create movement table
+    % line 1 demonstrates movement type 
+    % value 0 for x - u type + v - x type
+    % value 1 for x - u type
+    % value 2 for v - x type
+    % value 3 for x1 - x2 type
+    % line 2 demonstrates movement value
+    % line 3 demonstrates movement place
+    % value n for place n
+    % value -1 for not taking a place
+    movement_table = zeros(3, size(c, 2));
+    place = size(c, 2) + 1;
+    for i = 1: size(c, 2)
+        if (ub(i) - lb(i) < 0)
+            error("lowerbound is greater than upperbound.");
+        end       
+        if (lb(i) ~= -inf) && (ub(i) ~= +inf)
+            movement_table(1, i) = 0;
+            movement_table(2, i) = ub(i) - lb(i);
+            movement_table(3, i) = place;
+            place = place + 1;
+        elseif (lb(i) ~= -inf) && (ub(i) == +inf)
+            movement_table(1, i) = 1;
+            movement_table(2, i) = -lb(i);
+            movement_table(3, i) = -1;
+        elseif (lb(i) == -inf) && (ub(i) ~= +inf)
+            movement_table(1, i) = 2;
+            movement_table(2, i) = ub(i);
+            movement_table(3, i) = -1;
+        elseif (lb(i) ~= -inf) && (ub(i) ~= +inf)
+            movement_table(1, i) = 3;
+            movement_table(2, i) = 0;
+            movement_table(3, i) = place;
+            place = place + 1;
+        end
+    end
+    
+    movement_table
 end
