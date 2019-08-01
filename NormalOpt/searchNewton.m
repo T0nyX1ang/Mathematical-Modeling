2e43914e7ff6c77575d2a7d12a3504d1f2639701
Tony Xiang <33901373+T0nyX1ang@users.noreply.github.com>
2019-08-02 00:43:13 +0800

    Update searchNewton.m
    epsilon < 0 -> epsilon <= 0, fix typo

--- NormalOpt/searchNewton.m
+++ NormalOpt/searchNewton.m
@@ -1,7 +1,7 @@
 function point = searchNewton(funct, initial, epsilon)
     % 1-d search with Newton method
     % function should be C-2 smooth
-    if (epsilon < 0)
+    if (epsilon <= 0)
         error("epsilon must be greater than 0.");
     end
     
@@ -9,7 +9,7 @@ function point = searchNewton(funct, initial, epsilon)
     doubletangent = getDoubleTangent(funct);
     while (abs(tangent{1}(initial)) >= epsilon)
         if (doubletangent{1, 1}(initial) < epsilon)
-            warning("Bad conditioned tangent");
+            warning("ill conditioned tangent");
         end
         initial = initial - tangent{1}(initial) ./ doubletangent{1, 1}(initial);
     end
