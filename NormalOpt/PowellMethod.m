9d5dd583f618048da3b445467b2892f0f8a4df43
Tony Xiang <33901373+T0nyX1ang@users.noreply.github.com>
2019-08-05 00:08:24 +0800

    Update Powell and Powell-Sargent Method.
    Change variable order

--- NormalOpt/PowellMethod.m
+++ NormalOpt/PowellMethod.m
@@ -1,36 +1,35 @@
-function [xval, fval] = PowellMethod(funct, initial, dirMatrix, eps, step)
+function [xval, fval] = PowellMethod(funct, initial, epsilon, step, dirMatrix)
     % An implementation of Powell method.
     % dirMatrix: direction matrix of n-dimension line vectors
-    % eps: error value
+    % epsilon: error value
     % step: search step when finding a valid interval
     dimension = size(initial, 2);
     
     if (nargin == 2)
+        epsilon = 1e-6;
+        step = 1e-3;        
         dirMatrix = eye(dimension);
-        eps = 1e-6;
-        step = 1e-3;
     elseif (nargin == 3)
-        eps = 1e-6;
-        step = 1e-3;
+        step = 1e-3;        
+        dirMatrix = eye(dimension);
     elseif (nargin == 4)
-        step = 1e-3;
+        dirMatrix = eye(dimension);
     end
     if (norm(dirMatrix) == 0)
         error("Line vectors must be linear independent");
-    elseif (eps <= 0)
-        error("eps must be greater than 0");
+    elseif (epsilon <= 0)
+        error("epsilon must be greater than 0");
     elseif (step <= 0)
         error("step must be greater than 0");
     end
     
     prev_xval = inf;
     xval = initial;
-    while (norm(xval - prev_xval) >= eps)
+    while (norm(xval - prev_xval) >= epsilon)
         prev_xval = xval;
         for i = 1:dimension
             dec_funct = @(lambda) funct(xval + lambda * dirMatrix(i, :));
-            [start, stop] = searchValidInterval(dec_funct, 0, step);
-            lambda = searchGoldenMean(dec_funct, start, stop, eps);
+            lambda = UniversalSearch(dec_funct, 0, epsilon, step, 'goldenmean', 0, 1e15);
             xval = xval + lambda * dirMatrix(i, :);
             % Update dirMatrix at the same time
             if (i < dimension - 1)
@@ -41,12 +40,11 @@ function [xval, fval] = PowellMethod(funct, initial, dirMatrix, eps, step)
         % Construct final search direction
         direction = xval - prev_xval;
         dec_funct = @(lambda) funct(xval + lambda * direction);
-        [start, stop] = searchValidInterval(dec_funct, 0, step);
-        lambda = searchGoldenMean(dec_funct, start, stop, eps);
+        lambda = UniversalSearch(dec_funct, 0, epsilon, step, 'goldenmean', 0, 1e15);
         xval = xval + lambda * direction;
         % Update dirMatrix
         dirMatrix(dimension, :) = direction;
-        if (norm(dirMatrix) < eps)
+        if (norm(dirMatrix) < epsilon)
             warning("Direction matrix is badly conditioned, norm=" + norm(dirMatrix));
         end
     end
--- NormalOpt/PowellSargentMethod.m
+++ NormalOpt/PowellSargentMethod.m
@@ -1,38 +1,37 @@
-function [xval, fval] = PowellSargentMethod(funct, initial, dirMatrix, eps, step)
+function [xval, fval] = PowellSargentMethod(funct, initial, epsilon, step, dirMatrix)
     % An implementation of Powell-Sargent method.
     % dirMatrix: direction matrix of n-dimension line vectors
-    % eps: error value
+    % epsilon: error value
     % step: search step when finding a valid interval
     dimension = size(initial, 2);
     
     if (nargin == 2)
+        epsilon = 1e-6;
+        step = 1e-3;        
         dirMatrix = eye(dimension);
-        eps = 1e-6;
-        step = 1e-3;
     elseif (nargin == 3)
-        eps = 1e-6;
-        step = 1e-3;
+        step = 1e-3;        
+        dirMatrix = eye(dimension);
     elseif (nargin == 4)
-        step = 1e-3;
+        dirMatrix = eye(dimension);
     end
     if (norm(dirMatrix) == 0)
         error("Line vectors must be linear independent");
-    elseif (eps <= 0)
-        error("eps must be greater than 0");
+    elseif (epsilon <= 0)
+        error("epsilon must be greater than 0");
     elseif (step <= 0)
         error("step must be greater than 0");
     end
     
     prev_xval = inf;
     xval = initial;
-    while (norm(xval - prev_xval) >= eps)
+    while (norm(xval - prev_xval) >= epsilon)
         prev_xval = xval;
         max_dec = -inf;
         max_index = 0;
         for i = 1:dimension
             dec_funct = @(lambda) funct(xval + lambda * dirMatrix(i, :));
-            [start, stop] = searchValidInterval(dec_funct, 0, step);
-            lambda = searchGoldenMean(dec_funct, start, stop, eps);
+            lambda = UniversalSearch(dec_funct, 0, epsilon, step, 'goldenmean', 0, 1e15);
             now_dec = funct(xval) - funct(xval + lambda * dirMatrix(i, :));
             if (now_dec > max_dec)
                 max_dec = now_dec; % Record maximum decrement.
@@ -44,8 +43,7 @@ function [xval, fval] = PowellSargentMethod(funct, initial, dirMatrix, eps, step
         % Construct final search direction
         direction = xval - prev_xval;
         dec_funct = @(lambda) funct(xval + lambda * direction);
-        [start, stop] = searchValidInterval(dec_funct, 0, step);
-        lambda = searchGoldenMean(dec_funct, start, stop, eps);
+        lambda = UniversalSearch(dec_funct, 0, epsilon, step, 'goldenmean', 0, 1e15);
         xval = xval + lambda * direction;
         % Update dirMatrix
         if (abs(lambda) > sqrt((funct(xval) - funct(prev_xval)) / (max_dec)))
@@ -60,7 +58,7 @@ function [xval, fval] = PowellSargentMethod(funct, initial, dirMatrix, eps, step
             dirMatrix(dimension, :) = direction;
         end
         
-        if (norm(dirMatrix) < eps)
+        if (norm(dirMatrix) < epsilon)
             warning("Direction matrix is badly conditioned, norm=" + norm(dirMatrix));
         end
     end
