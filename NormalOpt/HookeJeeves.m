88b3d52dc39028bf623f408aa4c4c51552dcf9d1
Tony Xiang <33901373+T0nyX1ang@users.noreply.github.com>
2019-08-04 16:46:04 +0800

    Update HookeJeeves.m
    Change variable order and name

--- NormalOpt/HookeJeeves.m
+++ NormalOpt/HookeJeeves.m
@@ -1,49 +1,49 @@
-function [xval, fval] = HookeJeeves(funct, initial, dirMatrix, delta, alpha, beta, eps)
+function [xval, fval] = HookeJeeves(funct, initial, epsilon, dirMatrix, delta, alpha, beta)
     % An implementation of Hooke-Jeeves method.
+    % epsilon: error value    
     % dirMatrix: direction matrix of n-dimension line vectors
     % delta: initial step
     % alpha: step incremental factor
     % beta: step decremental factor
-    % eps: error value
     dimension = size(initial, 2);
     
     if (nargin == 2)
+        epsilon = 1e-6;
         dirMatrix = eye(dimension);
         delta = 1;
         alpha = 2;
         beta = 1 / 2;
-        eps = 1e-6;
     elseif (nargin == 3)
+        dirMatrix = eye(dimension);
         delta = 1;
         alpha = 2;
-        beta = 1 / 2;
-        eps = 1e-6;        
+        beta = 1 / 2;       
     elseif (nargin == 4)
+        delta = 1;
+        alpha = 2;
+        beta = 1 / 2;       
+    elseif (nargin == 5)
         alpha = 2;
         beta = 1 / 2;
-        eps = 1e-6;         
-    elseif (nargin == 5)
-        beta = 1 / 2;
-        eps = 1e-6;
     elseif (nargin == 6)
-        eps = 1e-6;
+        beta = 1 / 2;
     end
     if (size(dirMatrix, 1) ~= dimension || size(dirMatrix, 2) ~= dimension)
         error("Invalid direction matrix");
     elseif (delta <= 0)
         error("delta must be greater than 0");
-    elseif (alpha < 1)
+    elseif (alpha <= 1)
         error("alpha must be greater than 1");
     elseif (beta <= 0 || beta >= 1)
         error("beta must be between 0 and 1");
-    elseif (eps < 0)
-        error("eps must be greater than 0");
+    elseif (epsilon <= 0)
+        error("epsilon must be greater than 0");
     end
 
     
     xval = initial;
     yval = xval;
-    while (delta >= eps)
+    while (delta >= epsilon)
         prev_xval = xval;
         for i = 1:dimension
             upval = yval + delta * dirMatrix(i, :);
